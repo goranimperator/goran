6077e0642aa86e3fa0bf6a906dccc13019131515
= <=>
2018-01-09 19:30:38 +0100

    Added oh yeah to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5.4
+                    Oh yeah Laravel 5.4
                 </div>
 
                 <div class="links">
